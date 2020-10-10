ca30ad10678d5510a4f719a9fcfa0d0191f01e5e
yhetman <juliagetman5@gmail.com>
2020-10-11 01:23:04 +0300

    Script done!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # scripts
+
+## Currently we have: 
+--------
+1. Easier way for commit and push routine
+2. A script that shows current moon phase (@bluksho)
+3. Script that sorts my Download directory 
--- sort-downloads.py
+++ sort-downloads.py
@@ -2,43 +2,47 @@
 
 import os
 
-TG_PATH = "/home/yhetman/Downloads/Telegram Desktop/"
-TG_FILE_PATH = "/home/yhetman/Downloads/Telegram\ Desktop/"
-
 DWN_PATH = "/home/yhetman/Downloads/"
 
-download_files = os.listdir(DWN_PATH)
+def moving_files(path):
+    files_list = os.listdir(path)
 
-telegram_files = os.listdir(TG_PATH)
+    for file_name in files_list:
+       fpath = os.path.join(path, file_name)
+       if os.path.isdir(fpath):
+           print("Directory ", file_name, " !")
+           continue
+       _file_name = file_name.replace(" ", "-")
+       _file_name = _file_name.replace("(", "-")
+       _file_name = _file_name.replace(")", "-")
+       extension = os.path.splitext(file_name)[-1].lower()
+       if file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+           os.rename(fpath, "/home/yhetman/Pictures/" + _file_name)
+           print ("Picture moved", fpath)
+       elif extension in ['.xlsx', '.xls']:
+           os.rename(fpath, DWN_PATH + "xlsx_files/" + _file_name)
+           print("Excel file moved", fpath)
+       elif extension in ['.pptx', '.ppt']:
+           os.rename(fpath, DWN_PATH + "pptx_files/" + _file_name)
+           print("Power point file moved", fpath)
+       elif extension in ['.docx', '.doc', '.docs']:
+           os.rename(fpath, DWN_PATH + "doc_files/" + _file_name)
+           print("Document file moved", fpath)
+       else:
+           dir_name = str(extension[1:]) + "_files"
+           dir_path = DWN_PATH + dir_name
+           if os.path.exists(dir_path) == False:
+               os.mkdir(dir_path)
+           print(dir_path,"\t", dir_name)
+           os.rename(fpath, dir_path + '/' + _file_name)
 
-all_files = download_files + telegram_files
+
+def main():
+    TG_PATH = "/home/yhetman/Downloads/Telegram Desktop/"
+    moving_files(DWN_PATH)
+    moving_files(TG_PATH)
 
 
-for file_name in telegram_files:
-    _file_name = file_name.replace(" ", "-")
-    _file_name = _file_name.replace("(", "-")
-    _file_name = _file_name.replace(")", "-")
 
-    extension = os.path.splitext(file_name)[-1].lower()
-    fpath = os.path.join(TG_PATH, file_name)
-
-    if file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
-        os.rename(fpath, "/home/yhetman/Pictures/" + _file_name)
-        print ("Picture moved", fpath)
-    elif extension in ['.xlsx', '.xls']:
-        os.rename(fpath, DWN_PATH + "xlsx_files/" + _file_name)
-        print("Excel file moved", fpath)
-    elif extension in ['.pptx', '.ppt']:
-        os.rename(fpath, DWN_PATH + "pptx_files/" + _file_name)
-        print("Power point file moved", fpath)
-    elif extension in ['.docx', '.doc', '.docs']:
-        os.rename(fpath, DWN_PATH + "doc_files/" + _file_name)
-        print("Document file moved", fpath)
-    else:
-        dir_name = str(extension[1:]) + "_files"
-        dir_path = DWN_PATH + dir_name
-        if os.path.exists(dir_path) == False:
-            os.mkdir(dir_path)
-        print(dir_path,"\t", dir_name)
-        os.rename(fpath, dir_path + '/' + _file_name)
-
+if __name__ == "__main__":
+    main()
